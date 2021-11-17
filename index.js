6ed3a18af2f00f0258af351706c3510a3fd10738
Arushi Somani <arushisomani@berkeley.edu>
2021-11-17 14:08:58 -0800

    updating janet to use yelp data for recommendations

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const { botIntents, commands, prefix } = require('./config/config');
 
 const axios = require('axios');
 
-const foodType = [
-  "italian",
-  "mexican",
-  "korean",
-  "ramen",
-  "chinese",
-  "indian",
-  "thai",
-  "Taco Bell",
-  "Chipotle",
-  "Boba Ninja",
-  "salad",
-  "McDonalds",
-  "Racha's Cafe",
-  "Kimchi Garden",
-  "starve :)"
-]
-
 const client = new Client({
   intents: botIntents,
   partials: ['CHANNEL', 'MESSAGE'],
@@ -43,8 +25,18 @@ client.on('messageCreate', async (msg) => {
         msg.channel.send('Hi, there!');
     }
     else if(userCmd == commands.food) {
-        const random = foodType[Math.floor(Math.random()*foodType.length)];
-        msg.channel.send(`I've analysed ${Math.floor(Math.random()*1000000)} meal decisions you've made and decided that you should get **${random}**.`);
+
+        let res = await axios.get('https://api.yelp.com/v3/businesses/search', {
+          headers: {'Authorization': `Bearer ${process.env.YELP_TOKEN}`},
+          params: {'location': 'berkeley', 'open_now': true}
+        });
+
+        let restaurants = res.data.businesses;
+
+        const place = restaurants[Math.floor(Math.random()*restaurants.length)];
+        console.log(place);
+        const categories = place.categories.map((e) => (e.alias))
+        msg.channel.send(`I've analysed ${Math.floor(Math.random()*1000000)} meal decisions you've made and decided that you should get **${place.name}**. They're at ${place.location.address1}, have a rating of ${place.rating}/5 and serve ${categories.join(" and ")}.`);
     }
     else if(userCmd == commands.help) {
       msg.channel.send('Hello, there! Ask Janet for food recommendations using the `?food` command, for help through `?help`, and say hi by typing `?janet`');
