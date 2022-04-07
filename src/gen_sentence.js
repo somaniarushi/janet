74262696fdab933db2005462858b2e4e723b9da8
Arushi Somani <arushisomani@berkeley.edu>
2022-04-06 21:06:24 -0700

    fuck tokenS

--- src/gen_sentence.js
+++ src/gen_sentence.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.DISCORD_TOKEN,
+  apiKey: process.env.OPENAI_TOKEN,
 });
 const openai = new OpenAIApi(configuration);
 
